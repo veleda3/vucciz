8fde247081d18d4e7053ccad4231588c1d2503a9
Daniel Velez <velez.a.daniel@gmail.com>
2017-11-07 09:48:53 -0500

    deleted the database file

--- public/assets/spree/backend/theme-b42c8a71daec3f0333f84e506b4dd49f6a8f0b099e833debdce66b6868fa2188.js
+++
@@ -1,1 +0,0 @@
-function Theme(){this.init=function(){this.upload()},this.upload=function(){$("#theme_template_file").on("change",function(){$("#new_theme").submit()})}}$(document).ready(function(){(new Theme).init()});
